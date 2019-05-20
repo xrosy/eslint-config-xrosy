0230031d6e2b5a85c68930d4a1087eaa6f8e0571
za-chenzhenyuan <jason@chenzhenyuan.com>
2019-05-20 12:03:43 +0800

    add react/jsx-sort-default-props

--- src/configs/react/index.js
+++ src/configs/react/index.js
@@ -56,6 +56,7 @@ module.exports = {
     'react/jsx-one-expression-per-line'         : [ 0 ],
     'react/jsx-pascal-case'                     : [ 2 ],
     'react/jsx-props-no-spreading'              : [ 1, { html: 'enforce',custom: 'ignore' }],
+    'react/jsx-sort-default-props'              : [ 0 ],
     'react/jsx-sort-props'                      : [ 0 ],
     'react/jsx-tag-spacing'                     : [ 2, { afterOpening: 'never', beforeSelfClosing: 'always', closingSlash: 'never' }],
     'react/jsx-uses-react'                      : [ 2 ],
