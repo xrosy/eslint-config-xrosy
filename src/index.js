3c1438d3322e2187a026983d12301b3d714fac1b
za-chenzhenyuan <jason@chenzhenyuan.com>
2019-06-10 10:40:21 +0800

    update

--- src/index.js
+++ src/index.js
@@ -52,10 +52,20 @@ module.exports = {
     },
   },
 
-
   extends : gExtends,
 
+  plugins : [ 'babel' ],
+
   rules : {
+    'babel/new-cap'              : [ 1 ],
+    'babel/camelcase'            : [ 1 ],
+    'babel/no-invalid-this'      : [ 1 ],
+    'babel/object-curly-spacing' : [ 1 ],
+    'babel/quotes'               : [ 1 ],
+    'babel/semi'                 : [ 1 ],
+    'babel/no-unused-expressions': [ 1 ],
+    'babel/valid-typeof'         : [ 1 ],
+
     'no-console' : [ noConsoleValue, { allow: [ 'warn', 'error' ]}],
   },
 };
